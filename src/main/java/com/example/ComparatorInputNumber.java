ff81bfb8aad0ec0a1ea4ff1ea2e269af4fa5a192
jenkincstang <jenkincs@163.com>
2020-08-26 14:16:31 +0800

    [tdd-poker-hands][Lebron Jenkin] refactor: refactor the variable in method compare()

--- src/main/java/com/example/ComparatorInputNumber.java
+++ src/main/java/com/example/ComparatorInputNumber.java
@@ -4,10 +4,10 @@ import java.util.Comparator;
 
 public class ComparatorInputNumber implements Comparator {
 
-  public int compare(Object o1, Object o2) {
-    InputNumber number0 = (InputNumber) o1;
-    InputNumber number1 = (InputNumber) o2;
-    return number0.getValue().compareTo(number1.getValue());
+  public int compare(Object preObject, Object laterObject) {
+    InputNumber preNumber = (InputNumber) preObject;
+    InputNumber laterNumber = (InputNumber) laterObject;
+    return preNumber.getValue().compareTo(laterNumber.getValue());
   }
 
 }
--- src/test/java/com/example/CheckNumberTest.java
+++ src/test/java/com/example/CheckNumberTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DemoTest {
+public class CheckNumberTest {
 
   private static Demo demo;
 
