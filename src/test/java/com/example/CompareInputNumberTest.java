731a212ba8d5e7a00b51e2f6776e05d476ee14b9
jenkincstang <jenkincs@163.com>
2020-08-30 14:31:47 +0800

    [tdd poker hands][Lebron & Jenkin] feature: pass should_return_tie_when_compare_two_input_number_given_black_2H_4D_4S_2C_4D_and_white_2H_4D_4S_2C_4D

--- src/test/java/com/example/CompareInputNumberTest.java
+++ src/test/java/com/example/CompareInputNumberTest.java
@@ -42,6 +42,40 @@ public class CompareInputNumberTest {
     Assert.assertEquals(result,expectResult);
   }
 
+
+  @Test
+  void should_return_tie_when_compare_two_input_number_given_black_2H_4D_4S_2C_4D_and_white_2H_4D_4S_2C_4D(){
+    //given
+    InputNumber inputNumber1 = new InputNumber(2, "2", "H");
+    InputNumber inputNumber2 = new InputNumber(4, "4", "D");
+    InputNumber inputNumber3 = new InputNumber(4, "4", "S");
+    InputNumber inputNumber4 = new InputNumber(2, "2", "C");
+    InputNumber inputNumber5 = new InputNumber(4, "4", "D");
+    List<InputNumber> blackInputNumber = new ArrayList<InputNumber>();
+    blackInputNumber.add(inputNumber1);
+    blackInputNumber.add(inputNumber2);
+    blackInputNumber.add(inputNumber3);
+    blackInputNumber.add(inputNumber4);
+    blackInputNumber.add(inputNumber5);
+
+    InputNumber inputNumber11 = new InputNumber(2, "2", "H");
+    InputNumber inputNumber22 = new InputNumber(4, "4", "D");
+    InputNumber inputNumber33 = new InputNumber(4, "4", "S");
+    InputNumber inputNumber44 = new InputNumber(2, "2", "C");
+    InputNumber inputNumber55 = new InputNumber(4, "4", "D");
+    List<InputNumber> whiteInputNumber = new ArrayList<InputNumber>();
+    whiteInputNumber.add(inputNumber11);
+    whiteInputNumber.add(inputNumber22);
+    whiteInputNumber.add(inputNumber33);
+    whiteInputNumber.add(inputNumber44);
+    whiteInputNumber.add(inputNumber55);
+
+    String expectResult = "Tie";
+    //when
+    String result = CompareInputNumber.compareHighCard(blackInputNumber,whiteInputNumber);
+    //then
+    Assert.assertEquals(result,expectResult);
+  }
 //  @Test
 //  void should_return_black_wins_when_compare_two_input_number_given_black_2H_3D_5S_9C_KD_and_white_2C_3H_4S_8C_AH(){
 //    //given
@@ -75,6 +109,7 @@ public class CompareInputNumberTest {
 //    //then
 //    Assert.assertEquals(result,expectResult);
 //  }
+
   @Test
   void should_return_white_wins_when_compare_two_input_number_given_black_5H_5D_6S_7C_4D_and_white_2S_3S_8S_8S_AS(){
     //given
